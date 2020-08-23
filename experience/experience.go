56f0aa2e3ace7a34e732c71256e0fe169d8b1944
ntidi <62829698+ntidi@users.noreply.github.com>
2020-08-23 17:51:17 +0700

    Create experience.go

---
+++ experience/experience.go
@@ -0,0 +1,27 @@
+package experience
+
+var jobs []map[string]string
+
+func init() {
+	jobs = []map[string]string{
+		{
+			"from":                "2010",
+			"to":                  "2012",
+			"company":             "ABC",
+			"position":            "Developer",
+			"programing language": "Ruby",
+		},
+		{
+			"from":                "2012",
+			"to":                  "now",
+			"company":             "XYZ",
+			"position":            "Architecture",
+			"programing language": "Go, Java, Prolog, Smalltalk",
+		},
+	}
+}
+
+// Jobs return working experiences
+func Jobs() []map[string]string {
+	return jobs
+}
